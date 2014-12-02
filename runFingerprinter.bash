634b188186d068d3f2cf16f3c28ce77f50b10ca4
Andrew <jamfan16@gmail.com>
2014-12-02 13:55:25 -0500

    Fixed order.

--- runFingerprinter.bash
+++ runFingerprinter.bash
@@ -2,6 +2,6 @@
 find "netflix" -type f | grep ".xml" | while read line
 do
   path=`dirname $line`
-  grep "range/0-" $line | sort | uniq | cut -d "\"" -f2 > $path"/urls.txt"
+  grep "range/0-" $line | cut -d "\"" -f2 | sort | uniq > $path"/urls.txt"
 done
 find "netflix" -type d -links 2 | java -jar Fingerprinter.jar
